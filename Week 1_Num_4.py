33779ba238d03625720a547ba9cfaea370b7fea0
Mathis-NY <mathiscollignon@gmail.com>
2025-01-22 00:56:32 -0500

    Week 1_Num_4.py

---
+++ Week 1_Num_4.py
@@ -0,0 +1,11 @@
+
+def main():
+    x= int(input("m: "))
+    print ("E: ", calc (x))
+
+# on doit definir square
+def calc (n):
+    return n * 300000000**2
+
+#le main en bas permet de fermer la fonction
+main()
